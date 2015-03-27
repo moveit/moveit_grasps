c6b18ee5226812b3b5b669bf39960ace59b58ba3
Andy McEvoy <mcevoy.andy@gmail.com>
2015-03-26 22:48:09 -0600

    finished mesh bounding box

--- src/grasps.cpp
+++ src/grasps.cpp
@@ -655,4 +655,142 @@ bool Grasps::generateCuboidGrasps(const Eigen::Affine3d& cuboid_pose, float dept
     
 }
 
+bool Grasps::getBoundingBoxFromMesh(shape_msgs::Mesh mesh_msg, Eigen::Affine3d& pose, double& depth, double& width, double& height)
+{
+    int num_vertices = mesh_msg.vertices.size();
+    ROS_DEBUG_STREAM_NAMED("bbox","num triangles = " << mesh_msg.triangles.size());
+    ROS_DEBUG_STREAM_NAMED("bbox","num vertices = " << num_vertices);
+
+    // calculate centroid and moments of inertia
+    Eigen::Vector3d centroid = Eigen::Vector3d::Zero();
+    Eigen::Vector3d point = Eigen::Vector3d::Zero();
+    double Ixx, Iyy, Izz, Ixy, Ixz, Iyz;
+    Ixx = 0; Iyy = 0; Izz = 0; Ixy = 0; Ixz = 0; Iyz = 0;
+
+    for (int i = 0; i < num_vertices; i++)
+    {
+      // centroid sum
+      point << mesh_msg.vertices[i].x, mesh_msg.vertices[i].y, mesh_msg.vertices[i].z;
+      //point = visual_tools_->convertPose(mesh_pose_) * point;
+      centroid += point;
+
+      // moments of inertia sum
+      Ixx += point[1] * point[1] + point[2] * point[2];
+      Iyy += point[0] * point[0] + point[2] * point[2];
+      Izz += point[0] * point[0] + point[1] * point[1];
+      Ixy += point[0] * point[1];
+      Ixz += point[0] * point[2];
+      Iyz += point[1] * point[2];
+
+    }
+    
+    // final centroid calculation
+    for (int i = 0; i < 3; i++)
+    {
+      centroid[i] /= num_vertices;
+    }
+    ROS_DEBUG_STREAM_NAMED("bbox","centroid = \n" << centroid);
+
+    if (verbose_)
+      visual_tools_->publishSphere(centroid, rviz_visual_tools::PINK, 0.01);
+
+    // Solve for principle axes of inertia
+    Eigen::Matrix3d inertia_axis_aligned;
+    inertia_axis_aligned.row(0) <<  Ixx, -Ixy, -Ixz;
+    inertia_axis_aligned.row(1) << -Ixy,  Iyy, -Iyz;
+    inertia_axis_aligned.row(2) << -Ixz, -Iyz,  Izz;    
+
+    ROS_DEBUG_STREAM_NAMED("bbox","inertia_axis_aligned = \n" << inertia_axis_aligned);
+
+    Eigen::EigenSolver<Eigen::MatrixXd> es(inertia_axis_aligned);
+    
+    ROS_DEBUG_STREAM_NAMED("bbox","eigenvalues = \n" << es.eigenvalues());
+    ROS_DEBUG_STREAM_NAMED("bbox","eigenvectors = \n" << es.eigenvectors());
+    
+    Eigen::Vector3d axis_1 = es.eigenvectors().col(0).real();
+    Eigen::Vector3d axis_2 = es.eigenvectors().col(1).real();
+    Eigen::Vector3d axis_3 = es.eigenvectors().col(2).real();
+
+    // Test if eigenvectors are right-handed
+    Eigen::Vector3d w = axis_1.cross(axis_2) - axis_3;
+    double epsilon = 0.000001;
+    if ( !(std::abs(w(0)) < epsilon && std::abs(w(1)) < epsilon && std::abs(w(2)) < epsilon) )
+    {
+      axis_3 *= -1;
+      ROS_DEBUG_STREAM_NAMED("bbox","eigenvectors are left-handed, multiplying v3 by -1");
+    }
+
+    // assumes msg was given wrt world... probably needs better name
+    Eigen::Affine3d world_to_mesh_transform = Eigen::Affine3d::Identity();
+    world_to_mesh_transform.linear() << axis_1, axis_2, axis_3;
+    world_to_mesh_transform.translation() = centroid;
+    
+    // Transform and get bounds
+    Eigen::Vector3d min;
+    Eigen::Vector3d max;
+    for (int i = 0; i < 3; i++)
+    {
+      min(i)=std::numeric_limits<double>::max();
+      max(i)=std::numeric_limits<double>::min();
+    }
+
+    for (int i = 0; i < num_vertices; i++)
+    {
+      point << mesh_msg.vertices[i].x, mesh_msg.vertices[i].y, mesh_msg.vertices[i].z;
+      point = world_to_mesh_transform.inverse() * point;
+      for (int j = 0; j < 3; j++)
+      {
+        if (point(j) < min(j))
+          min(j) = point(j);
+
+        if (point(j) > max(j))
+          max(j) = point(j);
+      }
+    }
+    ROS_DEBUG_STREAM_NAMED("bbox","min = \n" << min);
+    ROS_DEBUG_STREAM_NAMED("bbox","max = \n" << max);
+
+    // points
+    Eigen::Vector3d p[8];
+    
+    p[0] << min(0), min(1), min(2);
+    p[1] << max(0), min(1), min(2);
+    p[2] << min(0), max(1), min(2); 
+    p[3] << max(0), max(1), min(2);
+
+    p[4] << min(0), min(1), max(2);
+    p[5] << max(0), min(1), max(2);
+    p[6] << min(0), max(1), max(2); 
+    p[7] << max(0), max(1), max(2);   
+
+    if (verbose_)
+    {
+      for (int i = 0; i < 8; i++)
+        visual_tools_->publishSphere(world_to_mesh_transform * p[i],rviz_visual_tools::YELLOW,0.01);
+    }
+
+    depth = max(0) - min(0);
+    width = max(1) - min(1);
+    height = max(2) - min(2);
+    ROS_DEBUG_STREAM_NAMED("bbox","bbox size = " << depth << ", " << width << ", " << height);
+    
+    Eigen::Affine3d cuboid_pose;
+    Eigen::Vector3d translation;
+    translation << (min(0) + max(0)) / 2.0, (min(1) + max(1)) / 2.0, (min(2) + max(2)) / 2.0;
+    ROS_DEBUG_STREAM_NAMED("bbox","bbox origin = \n" << translation);
+    cuboid_pose = world_to_mesh_transform;
+    cuboid_pose.translation() = world_to_mesh_transform * translation;
+    pose = cuboid_pose;
+
+    if (verbose_)
+    {
+      visual_tools_->publishCuboid(visual_tools_->convertPose(cuboid_pose),
+                                   depth,width,height,rviz_visual_tools::TRANSLUCENT);
+      visual_tools_->publishAxis(world_to_mesh_transform);
+    }
+
+    return true;
+}
+
+
 } // namespace
--- src/mesh_bounding_box_test.cpp
+++ src/mesh_bounding_box_test.cpp
@@ -3,21 +3,13 @@
   Desc:   Tests the grasps.cpp bounding box function
 */
 
-#include <ros/ros.h>
+#include <iostream>
 
 #include <moveit_visual_tools/moveit_visual_tools.h>
 #include <moveit_grasps/grasps.h>
 
-// #include <rviz_visual_tools/rviz_visual_tools.h>
-
-#include <geometric_shapes/shape_operations.h>
-#include <geometric_shapes/bodies.h>
-
-//#include <pcl/features/moment_of_inertia_estimation.h>
-//#include <pcl/common/common_headers.h>
-
 #include <boost/filesystem.hpp>
-#include <Eigen/Eigenvalues>
+#include <boost/foreach.hpp>
 
 namespace fs = boost::filesystem;
 
@@ -27,9 +19,9 @@ namespace moveit_grasps
 class MeshBoundingBoxTest
 {
 private:
-  moveit_visual_tools::MoveItVisualToolsPtr visual_tools_;
-  geometry_msgs::Pose mesh_pose_;
-
+moveit_grasps::GraspsPtr grasps_;
+moveit_visual_tools::MoveItVisualToolsPtr visual_tools_;
+  
 public:
 
   MeshBoundingBoxTest(int number_of_trials, bool verbose)
@@ -40,34 +32,44 @@ public:
     visual_tools_->setMuted(false);
     visual_tools_->loadMarkerPub();
     visual_tools_->deleteAllMarkers();
-
+    
     // seed random
     srand(ros::Time::now().toSec());
+    
+    // load grasp generator
+    grasps_.reset(new moveit_grasps::Grasps(visual_tools_, verbose) );
 
     int completed_trials = 0;
 
-    while(ros::ok())
+    ROS_INFO_STREAM_NAMED("test","\n*************\nStarting test\n*************");
+
+    std::string package_path = ros::package::getPath("picknik_main");
+    fs::path target_dir(package_path+ "/meshes/products/");
+    
+    fs::directory_iterator it(target_dir), eod;
+    ROS_DEBUG_STREAM_NAMED("bbox","Directory: " << target_dir.string());
+
+    double depth, width, height;
+    Eigen::Affine3d pose_oobb;
+    Eigen::Affine3d mesh_pose = Eigen::Affine3d::Identity();
+
+    // loop through each product mesh and test bounding box
+    BOOST_FOREACH(fs::path const &p, std::make_pair(it,eod))
     {
-      ROS_INFO_STREAM_NAMED("test","\n************* \nStarting test "
-                            << completed_trials + 1 << " of " << number_of_trials << "\n*************");
+      if (!ros::ok())
+        break;
+      
+      pose_oobb = Eigen::Affine3d::Identity();
+      depth = 0; width = 0; height = 0;
 
-      // get mesh file to publish
-      // TODO: grab a random mesh from the meshes directory
-      fs::path mesh_path = "file:/home/andy/ros/ws_picknik/src/picknik/picknik_main/meshes/products/kong_air_dog_squeakair_tennis_ball/recommended.dae";
+      fs::path file_name("recommended.stl");
+      fs::path mesh_path = p / file_name;
+      ROS_INFO_STREAM_NAMED("bbox","Processing mesh " << mesh_path.string());  
 
-      // get random pose for mesh
-      rviz_visual_tools::RandomPoseBounds bounds(0.0, 1.0, 0.0, 1.0, 0.0, 1.0);
-      visual_tools_->generateRandomPose(mesh_pose_,bounds );
-      ROS_INFO_STREAM_NAMED("test","Position = " << mesh_pose_.position.x << ", " <<
-                            mesh_pose_.position.y << ", " << mesh_pose_.position.z);
-      ROS_INFO_STREAM_NAMED("test","Quaternion = " << mesh_pose_.orientation.x << ", " <<
-                            mesh_pose_.orientation.y << ", " << mesh_pose_.orientation.z);
-
-      visual_tools_->publishMesh(mesh_pose_,mesh_path.string());
-
-      visual_tools_->publishAxis(mesh_pose_);
-      // GET BOUNDING BOX FUNCTION
-      shapes::Shape* mesh= shapes::createMeshFromResource(mesh_path.string());
+      ROS_DEBUG_STREAM_NAMED("bbox","creating mesh from file... ");
+      shapes::Shape* mesh= shapes::createMeshFromResource("file://" + mesh_path.string());
+ 
+      ROS_DEBUG_STREAM_NAMED("bbox","creating shape message...");
       shapes::ShapeMsg shape_msg;
       if (!mesh || !shapes::constructMsgFromShape(mesh, shape_msg))
       {
@@ -75,117 +77,54 @@ public:
       }
       shape_msgs::Mesh mesh_msg = boost::get<shape_msgs::Mesh>(shape_msg);
 
-      double depth, width, height;
-      computeBoundingBox(mesh_msg, depth, width, height);
+      ROS_DEBUG_STREAM_NAMED("bbox","getting bounding box...");
 
-      // END BOUNDING BOX FUNCTION
+      grasps_->getBoundingBoxFromMesh(mesh_msg, pose_oobb, depth, width, height); // this introduces a huge delay
+      visual_tools_->publishMesh(mesh_pose, "file://" + mesh_path.string());
 
-      completed_trials++;
-      if (completed_trials == number_of_trials)
-        break;
+      std::cin.get();
+      visual_tools_->deleteAllMarkers();
     }
 
-  }
-
-  void computeBoundingBox(shape_msgs::Mesh mesh_msg, double& depth, double& width, double& height)
-  {
-
-    int num_vertices = mesh_msg.vertices.size();
-    ROS_DEBUG_STREAM_NAMED("bbox","num triangles = " << mesh_msg.triangles.size());
-    ROS_DEBUG_STREAM_NAMED("bbox","num vertices = " << num_vertices);
-
-    // calculate centroid and moments of inertia
-    Eigen::Vector3d centroid = Eigen::Vector3d::Zero();
-    Eigen::Vector3d point = Eigen::Vector3d::Zero();
-    double Ixx, Iyy, Izz, Ixy, Ixz, Iyz;
-    Ixx = 0; Iyy = 0; Izz = 0; Ixy = 0; Ixz = 0; Iyz = 0;
-
-    for (int i = 0; i < num_vertices; i++)
-    {
-      // centroid sum
-      point << mesh_msg.vertices[i].x, mesh_msg.vertices[i].y, mesh_msg.vertices[i].z;
-      point = visual_tools_->convertPose(mesh_pose_) * point;
-      centroid += point;
-
-      // moments of inertia sum
-      Ixx += point[1] * point[1] + point[2] * point[2];
-      Iyy += point[0] * point[0] + point[2] * point[2];
-      Izz += point[0] * point[0] + point[1] * point[1];
-      Ixy += point[0] * point[1];
-      Ixz += point[0] * point[2];
-      Iyz += point[1] * point[2];
-
-    }
-    
-    // final centroid calculation
-    for (int i = 0; i < 3; i++)
-    {
-      centroid[i] /= num_vertices;
-    }
-    ROS_DEBUG_STREAM_NAMED("centroid","centroid = \n" << centroid);
-    visual_tools_->publishSphere(centroid, rviz_visual_tools::PINK, 0.01);
-
-    // Solve for principle axes of inertia
-    Eigen::Matrix3d inertia_axis_aligned;
-
-    inertia_axis_aligned.row(0) <<  Ixx, -Ixy, -Ixz;
-    inertia_axis_aligned.row(1) << -Ixy,  Iyy, -Iyz;
-    inertia_axis_aligned.row(2) << -Ixz, -Iyz,  Izz;    
-
-    ROS_DEBUG_STREAM_NAMED("inertia","inertia_axis_aligned = \n" << inertia_axis_aligned);
-
-    Eigen::EigenSolver<Eigen::MatrixXd> es(inertia_axis_aligned);
-    
-    ROS_DEBUG_STREAM_NAMED("inertia","eigenvalues = \n" << es.eigenvalues());
-    ROS_DEBUG_STREAM_NAMED("inertia","eigenvectors = \n" << es.eigenvectors());
-
-
-    
-  }
-
-  double fRand(double fMin, double fMax)
-  {
-    return fMin + ( (double)rand() / RAND_MAX ) * (fMax - fMin);
-  }
-
+}
 
 }; // class
 
 } // namespace
 
- int main(int argc, char** argv)
+int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "mesh_bounding_box_test");
+ros::init(argc, argv, "mesh_bounding_box_test");
 
-  int number_of_trials = 1;
-  bool verbose = false;
+int number_of_trials = 1;
+bool verbose = true;
 
-  if (argc > 1)
-  {
-    for (std::size_t i = 0; i < argc; i++)
-    {
-      if (strcmp(argv[i], "--verbose") == 0)
-      {
-        i++;
-        if (strcmp(argv[i], "true") == 0 )
-        {
-          ROS_INFO_STREAM_NAMED("main","Running in VERBOSE mode (slower)");
-          verbose = true;
-        }
-        continue;
-      }
+if (argc > 1)
+ {
+for (std::size_t i = 0; i < argc; i++)
+ {
+if (strcmp(argv[i], "--verbose") == 0)
+ {
+i++;
+if (strcmp(argv[i], "true") == 0 )
+ {
+ROS_INFO_STREAM_NAMED("main","Running in VERBOSE mode (slower)");
+verbose = true;
+}
+continue;
+}
 
-      if (strcmp(argv[i], "--trials" ) == 0)
-      {
-        i++;
-        number_of_trials = std::atoi(argv[i]);
-        continue;
-      }
-    }
-    ROS_INFO_STREAM_NAMED("main","Will run " << number_of_trials << " trials");
-  }
+if (strcmp(argv[i], "--trials" ) == 0)
+ {
+i++;
+number_of_trials = std::atoi(argv[i]);
+continue;
+}
+}
+ROS_INFO_STREAM_NAMED("main","Will run " << number_of_trials << " trials");
+}
 
-  moveit_grasps::MeshBoundingBoxTest tester(number_of_trials, verbose);
+moveit_grasps::MeshBoundingBoxTest tester(number_of_trials, verbose);
 
-  return 0;
+return 0;
 }
